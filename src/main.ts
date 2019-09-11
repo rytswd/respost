5b4c4871a976d45f3a21440d76547fa958d4357a
Ryota <rytswd@gmail.com>
2019-09-11 23:46:23 +0100

    Add initial implementation of respost

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,47 @@
-import * as core from '@actions/core';
+import { getInput, setOutput, setFailed } from "@actions/core";
+import { readFileSync } from "fs";
+import request from "request";
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const title = getInput("title");
+    const body = getInput("body");
+
+    const message = { body: "> " + title + "\n\n" + body };
+
+    const url = await getAddress();
+
+    const options = {
+      url: url,
+      method: "POST",
+      headers: {
+        Authorization: "token $GITHUB_TOKEN",
+        "Content-Type": "application/json"
+      },
+      postData: message
+    };
+
+    request(options, (err, im, res) => {
+      console.log("respost sent, status: ", err);
+    });
   } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
   }
+}
+
+function getAddress(): string {
+  const path = process.env.GITHUB_EVENT_PATH;
+  if (!path) throw "GITHUB_EVENT_PATH not found";
+
+  const data = readFileSync(path);
+  const event = JSON.parse(data.toString());
+
+  let url = (event.issue && event.issue.comments_url) || null;
+  if (!url)
+    url = (event.pull_request && event.pull_request.comments_url) || null;
+  if (!url) throw "API endpoint for adding a comment not found";
+
+  return url;
 }
 
 run();
